b4039edcefe415013ad727c666e912b6378f2224
Paulo Eduardo Marchini Nery <nery@titi.net.br>
2016-09-08 18:42:33 -0300

    Add files via upload

---
+++ _tst/phpfunction/configuracao_homolog.php
@@ -0,0 +1,6 @@
+<?php
+$host		=	"localhost";              //endere�o do seu servidor MySQL
+$database	=	"titinet_homolog";        //o database que conter� sua tabela, muitas vezes seu pr�prio login
+$login_db	=	"titinet_homolog";        //login usado no MySQL
+$senha_db	=	"r8bCybqcc=7W";           //senha usado no MySQL
+?>

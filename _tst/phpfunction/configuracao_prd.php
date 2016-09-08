103478bff4d0186704d9d040d2d86cc722bb9ef4
Paulo <Paulo@nery>
2016-09-08 19:06:23 -0300

    new commit

---
+++ _tst/phpfunction/configuracao_prd.php
@@ -0,0 +1,6 @@
+<?php
+$host		=	"localhost";            //endere�o do seu servidor MySQL
+$database	=	"titinet_titi";         //o database que conter� sua tabela, muitas vezes seu pr�prio login
+$login_db	=	"titinet_titidb";       //login usado no MySQL
+$senha_db	=	"rnT86cns0kkwQx6AUR";   //senha usado no MySQL
+?>

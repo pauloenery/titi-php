2ac32af3b2104aa6e84252fa7e8b0b9cd1113b2c
Paulo <Paulo@nery>
2016-10-20 21:56:16 -0200

    New functions

--- _tst/phpfunction/userfunction.php
+++ _tst/phpfunction/userfunction.php
@@ -1,5 +1,80 @@
 <?php
 
+function perfilespecialista($perfilespecialista) {
+    if ($perfilespecialista == 2) {
+        return 'Pediatria';
+    } elseif ($perfilespecialista == 4) {
+        return 'Adulto';
+    } elseif ($perfilespecialista == 5) {
+        return 'Idoso';
+    }
+}
+
+function habilidade($habilidade) {
+    if ($habilidade == 3) {
+        return 'Alta Complexidade';
+    } elseif ($habilidade == 2) {
+        return 'Média Complexidade';
+    } elseif ($habilidade == 1) {
+        return 'Baixa Complexidade';
+    }
+}
+
+function ativo($ativo) {
+    if ($ativo == 1) {
+        return "Desativar";
+    } else {
+        return "Ativar";
+    }
+}
+
+function no_ativo($ativo) {
+    if ($ativo == 1) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function cargo($cargo) {
+
+    if ($cargo == "1") {
+        return 'Auxiliar/Técnico enfermagem';
+    } else if ($cargo == "2") {
+        return 'Enfermeiro';
+    } else if ($cargo == "3") {
+        return 'Cuidador';
+    } else if ($cargo == "4") {
+        return 'Fisioterapeuta';
+    } else if ($cargo == "5") {
+        return 'Fonoaudiólogo';
+    } else {
+        return 'Não informado';
+    }
+}
+
+function periodo($periodo) {
+
+    if ($periodo == "1") {
+        return 'Diurno';
+    } else if ($periodo == "2") {
+        return 'Noturno';
+    } else {
+        return 'Não informado';
+    }
+}
+
+function nome($nome) {
+
+    $pos = stripos($nome, " ");
+
+    if ($pos === false) {
+        return $nome;
+    } else {
+        return substr($nome, 0, $pos);
+    }
+}
+
 function setuser($user) {
     $cookie_name = "user";
     $cookie_value = $user;

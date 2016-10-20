1414a575ebd9c60ab5b0a78f4a19f374847f3bb9
Paulo <Paulo@nery>
2016-10-20 21:55:59 -0200

    New functions security

--- _tst/phpfunction/header_1.php
+++ _tst/phpfunction/header_1.php
@@ -1,31 +1,30 @@
 <?php
-
 /*
-  if($_SERVER['HTTP_ORIGIN'] == "http://titi.net.br")
-  {
-  header("Content-Type: application/json; charset=utf-8");
-  header('Access-Control-Allow-Origin: http://titi.net.br');
-  header('Access-Control-Allow-Headers: Origin, Content-Type');
+if ($_SERVER['HTTP_HOST'] == "titi.net.br") {
+    header("Content-Type: application/json; charset=utf-8");
+    header('Access-Control-Allow-Origin: http://titi.net.br');
+    header('Access-Control-Allow-Headers: Origin, Content-Type');
+} else {
+    header('Content-Type: text/html');
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Mensagem</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<p>Este recurso foi acessado de maneira ilegal:"
+        . "<ul>"
+        . "<li>Se acessado de <code>http://titi.net.br</code> ele retorna documento JSON </li>"
+        . "<li>Se acessado a partir de qualquer outra origem, "
+        . "ou simplesmente digitando o URL na barra de endereços do navegador, "
+        . "você recebe esse documento HTML</li>"
+        . "</ul>";
+    echo "</body>";
+    echo "</html>";
 
-  }
-  else
-  {
-  header('Content-Type: text/html');
-  echo "<html>";
-  echo "<head>";
-  echo "   <title>Mensagem</title>";
-  echo "</head>";
-  echo "<body>",
-  "<p>Este recurso comporta-se de duas maneiras:";
-  echo "<ul>",
-  "<li>Se acessado de <code>http://titi.net.br</code> ele retorna documento JSON </li>";
-  echo " <li>Se acessado a partir de qualquer outra origem, ou simplesmente digitando o URL na barra de endereços do navegador,";
-  echo "você recebe esse documento HTML</li>",
-  "</ul>",
-  "</body>",
-  "</html>";
-  }
- */
+    exit('Programa abortado');
+}
+*/
+
 
 header("Content-Type: application/json; charset=utf-8");
 header('Access-Control-Allow-Origin: *');

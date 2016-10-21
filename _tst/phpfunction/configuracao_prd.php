9ade9dc8548e5838a27b5c4cde147c77afd0a07a
Paulo <Paulo@nery>
2016-10-20 22:06:21 -0200

    remover credenciais de banco de dados

--- _tst/phpfunction/configuracao_prd.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host		=	"localhost";            //endere�o do seu servidor MySQL
-$database	=	"titinet_titi";         //o database que conter� sua tabela, muitas vezes seu pr�prio login
-$login_db	=	"titinet_titidb";       //login usado no MySQL
-$senha_db	=	"rnT86cns0kkwQx6AUR";   //senha usado no MySQL
-?>

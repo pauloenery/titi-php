4d95fd789d7e2a4a2478c513174b3b3d2a0bf055
Paulo Eduardo Marchini Nery <nery@titi.net.br>
2017-01-09 22:49:35 -0200

    Validação Data de Nascimento

--- _tst/phpfunction/userfunction.php
+++ _tst/phpfunction/userfunction.php
@@ -140,4 +140,28 @@ function checkperfil() {
     return $checkperfil;
 }
 
+function calc_idade($data_nasc) {
+
+    $data_nasc = explode("/", $data_nasc);
+
+    $data = date("d/m/Y");
+
+    $data = explode("/", $data);
+
+    $anos = $data[2] - $data_nasc[2];
+
+    if ($data_nasc[1] >= $data[1]) {
+
+        if ($data_nasc[0] <= $data[0]) {
+
+            return $anos;
+        } else {
+
+            return $anos - 1;
+        }
+    } else {
+
+        return $anos;
+    }
+}
 ?>

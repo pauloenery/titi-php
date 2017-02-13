fe92c5c04c44b7944156d4fa54957d2846d5e0bf
Paulo Eduardo Marchini Nery <nery@titi.net.br>
2017-02-13 20:40:36 -0200

    Alteração Nome log diario

--- _tst/phpfunction/geralog.php
+++ _tst/phpfunction/geralog.php
@@ -1,11 +1,11 @@
 <?php
 
 function geralog($msg, $prg) {
-    $file = '../log/TitiCuidadores.log';
+    $file = '../log/TitiCuidadores.'.date("Y-m-d").'.log';
 // $entityBody = file_get_contents('./contato.json', true);
 // Open the file to get existing content
     $current = '';
-// Append a new person to the file
+// Append a new message to the file
     $current .= date("Y-m-d")." ".date("H:i:s")." - ".$prg.' - '.$msg.chr(10);
 // Write the contents back to the file
     file_put_contents($file, $current, FILE_APPEND);

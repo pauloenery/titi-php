1f75f4481884c3790903db9a036427f9778482ae
Paulo Eduardo Marchini Nery <nery@titi.net.br>
2017-02-17 00:36:59 -0200

    Formulario avaliação de Profissionais  por Pacientes

---
+++ _tst/evaluate/evaluate.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+        <TITLE>TITI - Cuidadores cuidadosos</TITLE>
+        <META NAME="DESCRIPTION" CONTENT="Sabe o amor que você sente por uma pessoa? A gente sabe. Encontre Cuidadores, Enfermeiras e Fisioterapeutaas  para cuidar de quem você ama.">
+        <META NAME="ABSTRACT" CONTENT="Sabe o amor que você sente por uma pessoa? A gente sabe.">
+        <META NAME="KEYWORDS" CONTENT="Cuidadores, Cuidador, Idosos, Lar, Cuidados, Pessoas Especiais, Deficientes, Cuidador de Idoso, Deficientes, Cuidadores de Idosos, Cuidadores de Pessoas com Deficiência, Encontrar Cuidador, Buscador, Encontre Cuidadores, Buscar, Buscar Cuidador, Zona Leste, Zona Sul, Zona Oeste, Cuidador Zona Norte, Cuidador de Idosos em São Paulo, Cuidadores SP, cuidadores de idosos, assistência ao idoso hospitalizado, cuidador de bebe, cuidador de criança, cuidador de crianças, cuidador de deficientes, cuidador de idosos, cuidador especial, cuidadora, cuidadores, cuidadores de criança com deficiencia, cuidadores de idosos, cuidadores de pessoas, cuidadores de pessoas com alzheimer, cuidadores de pessoas com avc, cuidadores de pessoas com deficiencia atendidas em casa, cuidadores de pessoas com deficiencias, cuidadores de pessoas com necessidades especiais, cuidadores no lar, cuidados com paciente acamado, cuidados de pessoas idosas, cuidar de pessoas, cuidar de pessoas com alzheimer, cuidar de pessoas doentes, cuidar de pessoas idosas, cuidar de pessoas idosas acamadas, idoso acamado, idoso acamado com avc, fisioterapia, idosos, paciente acamado, paciente acamado cuidados, profissional cuidador, profissional cuidador de idoso, profissional cuidador de idosos">
+        <META NAME="ROBOT" CONTENT="All">
+        <META NAME="RATING" CONTENT="general">
+        <META NAME="DISTRIBUTION" CONTENT="global">
+        <meta http-equiv="content-language" content="pt-br" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta name="keywords" content="Cuidadores, Cuidador, Idosos, Lar, Cuidados, Pessoas Especiais, Deficientes, Cuidador de Idoso, Deficientes, Cuidadores de Idosos, Cuidadores de Pessoas com Deficiência, Encontrar Cuidador, Buscador, Encontre Cuidadores, Buscar, Buscar Cuidador, Zona Leste, Zona Sul, Zona Oeste, Cuidador Zona Norte, Cuidador de Idosos em São Paulo, Cuidadores SP, cuidadores de idosos, assistência ao idoso hospitalizado, cuidador de bebe, cuidador de criança, cuidador de crianças, cuidador de deficientes, cuidador de idosos, cuidador especial, cuidadora, cuidadores, cuidadores de criança com deficiencia, cuidadores de idosos, cuidadores de pessoas, cuidadores de pessoas com alzheimer, cuidadores de pessoas com avc, cuidadores de pessoas com deficiencia atendidas em casa, cuidadores de pessoas com deficiencias, cuidadores de pessoas com necessidades especiais, cuidadores no lar, cuidados com paciente acamado, cuidados de pessoas idosas, cuidar de pessoas, cuidar de pessoas com alzheimer, cuidar de pessoas doentes, cuidar de pessoas idosas, cuidar de pessoas idosas acamadas, idoso acamado, idoso acamado com avc, fisioterapia, idosos, paciente acamado, paciente acamado cuidados, profissional cuidador, profissional cuidador de idoso, profissional cuidador de idosos" />
+
+        <!-- CSS  -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Oleo+Script:400,700&subset=latin-ext" rel="stylesheet">
+        <link rel="stylesheet" href="t3.css">
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })
+                    (window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+            /*ga('create', 'UA-84850577-1', 'none');*/
+            ga('create', 'UA-84850577-1');
+            /*ga('send', 'pageview');*/
+
+        </script>
+
+        <!-- Google Tag Manager -->
+        <script>
+            function valida()
+            {
+                if (document.evaluate.especialistasID.value === "0" || document.evaluate.myclass.value === "" || document.evaluate.nomepaciente.value === "" || document.evaluate.emailpaciente.value === "")
+                {
+                    alert("Todos os campos com asterisco são de preenchimento obrigatório!");
+                    return(false);
+                }
+                else
+                {
+                    if (checarEmail()){
+                        return(true)
+                    }else{
+                        return(false)
+                    }
+                }
+            }
+
+
+
+        </script>
+        <script>
+            
+            function checarEmail() {
+                if (document.evaluate.emailpaciente.value === ""
+                        || document.evaluate.emailpaciente.value.indexOf('@') === -1
+                        || document.evaluate.emailpaciente.value.indexOf('.') === -1)
+                {
+                    alert("Por favor, informe um E-MAIL válido!");
+                    //document.forms[0].emailpaciente.select();
+                    //document.forms[0].emailpaciente.focus();
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        </script>
+
+    </head>
+    <body>
+        <div class="w3-row-padding" >
+            <div style="background-color: #01D7BC">
+                <table >
+                    <tr align="left">
+                        <td width="1%"></td>
+                        <td width="4%">
+                            <a href="http://titicuidadores.com.br">  
+                                <img class="left logo1 tt-lo" width="40px" src="../../../web/assets/images/new-logo-titi.png" alt="" style="margin-top:8px;margin-left:10px;"/></a>
+                        </td>
+                        <td width="50%">
+                            <h4  style=" font-size:22px;   line-height: 15px; padding-top: 5px; color: #ffffff">
+                                <b>TITI</b>
+                                <br>
+                                <span style="font-size:12px;">Cuidadores cuidadosos</span>
+                            </h4>             
+                        </td>
+                        <td width="1%"></td>
+                        <td width="3%"></td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="w3-col w3-center w3-light-grey w3-text-grey ">
+                <h3 class="w3-jumbo">Classificação do profissional</h3>
+                <h6 class="w3-xlarge">É simples! Você levará apenas 2 minutos para completar a avaliação do profissional.</h6>
+                <form action="update_evaluate.php" name="evaluate" id="evaluate"  onSubmit="return valida();">
+                    <table>
+                        <tr>
+                            <td width="50%" class="w3-center">
+                                <h6 class="w3-xlarge">* Qual o profissional que você esta avaliando? </h6>
+                                <h6 class="w3-xlarge">
+                                    <select name="especialistasID" id="especialistasID">
+                                        <?php profissionais(); ?>
+                                    </select>
+                                </h6>
+                            </td>
+                        </tr>
+                    </table>
+                    <h6 class="w3-xlarge">* Qual o Seu Nível de Satisfação? </h6>
+                    <table >
+
+                        <tr >
+                            <td width="20%"></td>
+                            <td width="20%">
+                                <h6 class="w3-xlarge">Pouco Satisfeito</h6></td>
+                            <td width="2%">
+                                <h6 class="w3-xlarge">1<br><input type="radio" name="myclass" id="myclass" value="1"></h6>
+                            </td>
+                            <td width="2%">
+                                <h6 class="w3-xlarge">2<br><input type="radio" name="myclass" id="myclass" value="2"></h6>
+                            </td>
+                            <td width="2%">
+                                <h6 class="w3-xlarge">3<br><input type="radio" name="myclass" id="myclass" value="3"></h6>
+                            </td>
+                            <td width="2%">
+                                <h6 class="w3-xlarge">4<br><input type="radio" name="myclass" id="myclass" value="4"></h6>
+                            </td>
+                            <td width="2%">
+                                <h6 class="w3-xlarge">5<br><input type="radio" name="myclass" id="myclass" value="5"></h6>
+                            </td>
+                            <td width="20%"><h6 class="w3-xlarge">Muito Satisfeito</h6></td>
+                            <td width="20%"></td>
+                        </tr>
+                    </table>
+                    <table>
+                        <tr>
+                            <td width="50%" class="w3-center">
+                                <h6 class="w3-xlarge">Mensagem sobre o profissional avaliado:</h6>
+                                <h6 class="w3-xlarge"><textarea rows="4" cols="50" size="50" maxlength="200" name="comentario" id="comentario"></textarea></h6>
+                            </td>
+                        </tr>
+                    </table>
+                    <h6 class="w3-xxxlarge">Seus Dados Pessoais</h6>
+                    <table>
+                        <tr>
+                            <td>
+                                <h6 class="w3-xlarge">* Seu Nome Completo:<br></h6>
+                                <h6 class="w3-xlarge"><input type="text" size="50" maxlength="50" name="nomepaciente" id="nomepaciente"></h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h6 class="w3-xlarge">* Seu e-mail pessoal: <br></h6>
+                                <h6 class="w3-xlarge"><input type="text" size="50" maxlength="50" name="emailpaciente" id="emailpaciente" onblur="checarEmail();"></h6>
+                            </td>
+                        </tr>
+                    </table>
+                    <h4 class="w3-xxlarge"><input type="submit" name="Enviar"></h4>
+
+                </form>
+            </div>
+        </div>
+
+    </body>
+</html>
+<?php
+
+Function profissionais() {
+    include "../phpfunction/configuracao.php";
+    $db = mysql_connect($host, $login_db, $senha_db);
+    $basedados = mysql_select_db($database);
+    $queryselect = "
+        SELECT
+            especialistas.especialistasID, usuarios.nome
+        FROM
+            especialistas
+        INNER JOIN
+            usuarios
+        ON
+            especialistas.usuariosID=usuarios.usuariosID
+        where
+            perfilID=2 and disponibilidade=1 and ativo=1 ORDER BY usuarios.nome ASC";
+    $resultselect = mysql_query($queryselect, $db);
+    echo'<option value="0">Escolha um profissional...</option>';
+    while ($linha = mysql_fetch_array($resultselect)) {
+        $espacialistasID = $linha["especialistasID"];
+        $nome = $linha["nome"];
+        echo'<option value="' . $espacialistasID . '">' . $nome . '</option>';
+    }
+}
+?>
---
+++ _tst/evaluate/update_evaluate.php
@@ -0,0 +1,172 @@
+<?php
+
+//include "../phpfunction/header_1.php";
+//include "../phpfunction/geralog.php";
+include "../phpfunction/configuracao.php";
+include "../phpfunction/userfunction.php";
+
+$tabela = "usuarios";     //o nome de sua tabela
+$db = mysql_connect($host, $login_db, $senha_db);
+$basedados = mysql_select_db($database);
+
+$especialistasID = $_REQUEST ["especialistasID"];
+$nome = $_REQUEST ["nomepaciente"];
+$email = $_REQUEST ["emailpaciente"];
+$myclass = (isset($_REQUEST ["myclass"]) ? $_REQUEST ["myclass"] : 0);
+$comentario = $_REQUEST ["comentario"];
+
+//echo 'especialistasID: ' . $especialistasID . ' - ';
+//echo 'nomepaciente: ' . $nome . ' - ';
+//echo 'emailpaciente: ' . $email . ' - ';
+//echo 'myclass: ' . $myclass . ' - ';
+//echo 'comentario: ' . $comentario . ' - ';
+if (usuariosID($email)) {
+    echo("<script>alert('Email inválido : $email ');</script>");
+    echo("<script>history.back();</script>");
+} elseif (($nome != '') || ($email != '') || ($myclass != 0)) {
+    $pacientesID = pacientesID($email);
+    IF ($pacientesID == 0) {
+        //echo 'mensagem 2: ' . $pacientesID;
+        $usuariosID = novo_usuario(3, $nome, $email);
+        $pacientesID = novo_paciente($usuariosID);
+    }
+    IF ($pacientesID != 0) {
+        update_classificacao($especialistasID, $pacientesID, $myclass, $comentario);
+    }
+    echo("<script>alert('Obrigado por colaborar com a sua avaliação ');</script>");
+    echo("<script>location.href = 'http://titicuidadores.com.br';</script>");
+}
+
+Function usuariosID($email) {
+    global $db, $basedados;
+    $queryselect = "
+        SELECT usuariosID
+        FROM usuarios 
+        where email='$email' and perfilID=2";
+    $resultselect = mysql_query($queryselect, $db) or die($queryselect . "<br/><br/>" . mysql_error());
+    $contagem = mysql_num_rows($resultselect);
+    if ($contagem == 1) {
+        return (TRUE);
+    } else {
+        return (FALSE);
+    }
+}
+
+Function pacientesID($email) {
+    global $db, $basedados;
+    $queryselect = "
+        SELECT pacientesID
+        FROM pacientes 
+        INNER JOIN usuarios
+        ON pacientes.usuariosID=usuarios.usuariosID
+        where email='$email'";
+    $resultselect = mysql_query($queryselect, $db) or die($queryselect . "<br/><br/>" . mysql_error());
+    $pacientesID = 0;
+    while ($linha = mysql_fetch_array($resultselect)) {
+        $pacientesID = $linha["pacientesID"];
+    }
+    //echo 'mensagem 1: ' . $pacientesID;
+
+    return ($pacientesID);
+}
+
+function novo_usuario($perfilID, $nome, $email) {
+    global $db, $basedados;
+
+    $tabela = "usuarios";     //o nome de sua tabela
+
+    $querynovousuario = "INSERT INTO `$tabela` (nome, email, perfilID)
+                                        VALUES ('$nome','$email',$perfilID)";
+    $cadastrar = mysql_query($querynovousuario, $db);
+    $sqlerro = mysql_errno($db) . ':' . mysql_error($db) . '\\n';
+    $last_inserted = mysql_insert_id();
+
+    return $last_inserted;
+}
+
+function novo_paciente($usuariosID) {
+    global $db, $basedados;
+    $tabela = "pacientes";     //o nome de sua tabela
+
+    $pesquisar = mysql_query("SELECT pacientesID FROM $tabela WHERE usuariosID = '$usuariosID'", $db);
+    $contagem = mysql_num_rows($pesquisar);
+
+    if ($contagem == 1) {
+        while ($linha = mysql_fetch_array($pesquisar)) {
+            $pacientesID = $linha["pacientesID"];
+        }
+    } else {
+        $querynovousuario = "INSERT INTO `$tabela` (usuariosID)
+                                        VALUES ($usuariosID)";
+
+        $cadastrar = mysql_query($querynovousuario, $db);
+        $last_inserted = mysql_insert_id();
+    }
+    return $last_inserted;
+}
+
+function update_classificacao($especialistasID, $pacientesID, $myclass, $comentario) {
+    global $db, $basedados;
+
+    $tabela = "especialistas_classificacao";     //o nome de sua tabela
+//Verifica se existe nota deste paciente para o profissional
+    $queryselect = "SELECT especialistas_classificacaoID, classificacao "
+            . "from $tabela "
+            . "WHERE "
+            . "especialistasID = $especialistasID and "
+            . "pacientesID = $pacientesID";
+    //echo $queryselect;
+
+    $resultselect = mysql_query($queryselect, $db);
+    $contagem = mysql_num_rows($resultselect);
+
+    if ($contagem == 1) {    //encontrou uma nota altera a classificação 
+        $queryupdate = "UPDATE $tabela SET "
+                . "`classificacao` = '$myclass' , "
+                . "`comentario` = '$comentario'  "
+                . "WHERE "
+                . "especialistasID = $especialistasID and "
+                . "pacientesID = $pacientesID";
+        $resultupdate = mysql_query($queryupdate, $db);
+    } else {                 // não encontrou insere uma classificação 
+        $queryinsert = "INSERT INTO `$tabela` (especialistasID, pacientesID, classificacao, comentario)
+                            VALUES ('$especialistasID','$pacientesID','$myclass','$comentario')";
+        $resultinsert = mysql_query($queryinsert, $db);
+    }
+    $queryselect = "SELECT count(*) AS total, AVG(classificacao) AS classificacao "
+            . "FROM "
+            . "$tabela "
+            . "where "
+            . "especialistasID = $especialistasID";
+    $resultselect = mysql_query($queryselect, $db);
+
+    $classificacaoDb = 0;
+    $totalDb = 0;
+    while ($linha = mysql_fetch_array($resultselect)) {
+        $classificacaoDb = $linha["classificacao"];
+        $totalDb = $linha["total"];
+    }
+
+
+    $tabela = "especialistas";     //o nome de sua tabela
+    $queryupdate = "UPDATE $tabela SET "
+            . "`classificacao` = '$classificacaoDb' , "
+            . "`total` = '$totalDb' "
+            . "WHERE "
+            . "especialistasID = $especialistasID";
+
+    $cadastrar = mysql_query($queryupdate, $db);
+    $retorno = array();
+    $i = 0;
+
+    if ($cadastrar == 1) {
+        $retorno[$i]["status"] = "OK";
+        $retorno[$i]["mensagem"] = "Classificação Alterado com sucesso";
+        $i++;
+    } else {
+        $retorno[$i]["status"] = "ERRO";
+        $retorno[$i]["mensagem"] = "Classificação não Alterada";
+        $i++;
+    }
+    return $retorno;
+}
